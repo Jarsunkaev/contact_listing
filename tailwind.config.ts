b55365404857dd5ba349c835381999cbe1c3dceb
Juszuf Arsunkaev <81538739+Jarsunkaev@users.noreply.github.com>
2024-08-27 23:27:20 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,23 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        glysa: ['Glysa', 'serif'],
+        'lexend-deca': ['Lexend Deca', 'sans-serif'],
+      },
+      fontWeight: {
+        thin: '100',
+        extralight: '200',
+        light: '300',
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800',
+        black: '900',
+      },
+    },
   },
   plugins: [],
 }
