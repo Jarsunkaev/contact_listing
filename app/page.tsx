5c78cd746b62ee2ef2cedfe9656fac31c8b006e8
Juszuf Arsunkaev <juszuf_1@icloud.com>
2024-08-28 18:18:51 +0200

    fixed border colours and header component

--- app/page.tsx
+++ app/page.tsx
@@ -20,10 +20,10 @@ export default function Home() {
       {/* Background element with borders */}
       <div className="fixed inset-0 pointer-events-none">
         <div className="h-full max-w-[720px] mx-auto relative">
-          <div className="absolute top-0 bottom-0 left-0 w-px bg-gray-700"></div>
-          <div className="absolute top-0 bottom-0 right-0 w-px bg-gray-700"></div>
+          <div className="absolute top-0 bottom-0 left-0 w-px bg-[#282828]"></div>
+          <div className="absolute top-0 bottom-0 right-0 w-px bg-[#282828]"></div>
         </div>
-        <div className="absolute top-20 left-0 right-0 h-px bg-gray-700"></div>
+        <div className="absolute top-20 left-0 right-0 h-px bg-[#282828]"></div>
       </div>
 
       {/* Main content */}
@@ -32,7 +32,7 @@ export default function Home() {
           <Header onOpenAddModal={() => setIsAddModalOpen(true)} />
         </div>
         {/* Full-width border */}
-        <div className="w-full h-px bg-gray-700"></div>
+        <div className="w-full h-px bg-[#282828]"></div>
         <div className="w-full max-w-[720px]">
           <ContactList contacts={contacts} />
         </div>
